1b1e8519c5e2a1de4556b72a143bf955ccea43c4
Lars Greiss <kufikugel@googlemail.com>
2013-12-02 01:49:52 +0100

    WIP QS tile customization
    still heavy WIP and only the basic stuff. Currently default
    all tiles are shown to test them till new settings for it is finished
    
    still tiles are missing
    battery till I think it completly broken did not test it till now lol
    
    so you can test but no guraranty if it will work at all :p
    
    PS2: QuietHours broadcast
    PS3: Context derp.
    PS4
    - revert QS battery tile implementation and move it into new
      Battery tile
    - move click action to longpress on alarmtile for more consistent
    - collapse qs for location tile to show the confirmation dialog
      when turning on
    PS5: change sleep screen icon back to our power icon and add longpress
         action to open power menu
    PS6: rename GpsTile into LocationTile and do not remove it on devices
         without GPS due that location is always available over wifi as well
         minor icon corrections
    PS8:
    - added expanded desktop
    - added Music tile
    - minor fixe
    PS9:
    - tweek the music tile...check the video
    http://slim.mimic.ca/images/music_tile.mp4
    
    PS10:
    - better controll of the picture stream on the music tile
    - send a broadcast if no bindable music callerId is present
      to force music player to start
    PS11:
    - fix location tile
    PS12:
    - add lincenses and changed on some tiles the click and lonpress behaviours
      to be consistent
    PS13:
    - tweek screen time out tile and added never as option to it
    PS14:
    - added reboot/recovery tile
    - added text color
    - tile quanitity per row including text resize
    - added tile bg color
    - added tile bg pressed color
    - added tile bg alpha
    - added langscape duplicate or not option
    - some tweeks and minor fixes
    PS15:
    - remove depreciated code
    - reboot tile add a small delay before calling final reboot
    - remove added padding....not needed any more with the new style
    PS16:
    - better settings support
    PS17:
    - add more dynamic tile layout to handle better 3,4,5 tiles in a row
    
    custom tile not sure if we do in initial version...lets see. Settings is a lot
    work due that I want to change a lot on our old version
    
    Other then that it seems to be done now :)
    
    so lets go doing the settings lol
    
    Change-Id: Ied33ca7d366b9792894c40cddcd2db649d69b902
    
    Conflicts:
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/values/dimens.xml

--- core/java/com/android/internal/util/slim/DeviceUtils.java
+++ core/java/com/android/internal/util/slim/DeviceUtils.java
@@ -75,6 +75,15 @@ public class DeviceUtils {
         return false;
     }
 
+    public static boolean deviceSupportsGps(Context context) {
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_LOCATION_GPS);
+    }
+
+    public static boolean deviceSupportsImeSwitcher(Context ctx) {
+        Resources res = ctx.getResources();
+        return res.getBoolean(com.android.internal.R.bool.config_show_IMESwitcher);
+    }
+
     public static boolean deviceSupportsTorch(Context context) {
         PackageManager pm = context.getPackageManager();
         Resources settingsResources = null;
